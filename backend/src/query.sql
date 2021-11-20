0d7e7792e611d5bb9f2c7de3a2c48022c723af91
Jaeseong Lee <jason.lee27@utexas.edu>
2021-11-20 10:47:29 -0600

    add query.sql

---
+++ backend/src/query.sql
@@ -0,0 +1,17 @@
+
+-- assumption:
+-- 1. one client has one trader
+-- 2. one trader can have several clients
+
+
+-- show transaction history of logged in clients (daily, weekly, monthly)
+
+-- show request history to logged in clients
+
+-- request bitcoin
+
+-- buy/sell bitcoin for clients and traders + log
+
+-- transfer money (us dollar) from client to trader + log
+
+-- cancel transaction for trader
