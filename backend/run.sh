376494d6af19c655d98fd8e2e32dd233224a175f
Jaeseong Lee <jason.lee27@utexas.edu>
2021-11-29 21:05:16 -0600

    run on multiple os

--- backend/run.sh
+++ backend/run.sh
@@ -3,18 +3,35 @@
 readonly _DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 readonly SRC_DIR="${_DIR}/src"
 readonly DB_INIT_FILE="${SRC_DIR}/tables.sql"
+readonly OS_TYPE="${OSTYPE}"
 
-function main() {
-        init_sql_tables
+function main() {        
+        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+                # linux
+                main_linux
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+                # Mac OSX
+                main_macos
+        fi
+}
+
+function main_linux() {
+        sudo service mysql start # start mysql server
+        # sudo service mysql status
+        sudo mysql -u root < ${DB_INIT_FILE}
+        (cd ${SRC_DIR}
+         python app.py
+        )
+        sudo service mysql stop # stop mysql server
+        # sudo service mysql status
+}
+function main_macos() {
+        mysql.server start
+        mysql -u root < ${DB_INIT_FILE}
         (cd ${SRC_DIR}
          python app.py
         )
         mysql.server stop
-}
-
-function init_sql_tables() {
-        mysql.server start
-        mysql -u root < ${DB_INIT_FILE}
 }
 
 main
