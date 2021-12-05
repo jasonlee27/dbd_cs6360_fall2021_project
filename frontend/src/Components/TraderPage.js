5bffca8d1e38429560ceb6766d6c639b003ef5ff
Gabe Goldstein <thegabe6@yahoo.com>
2021-12-05 14:30:37 -0600

    trader page update

--- frontend/src/Components/TraderPage.js
+++ frontend/src/Components/TraderPage.js
@@ -8,8 +8,26 @@ import ClientSearch from "./ClientSearch.js";
 function TraderPage(props) {
   return (
     <div>
-      <ClientSearch logout={props.logout} ></ClientSearch>
-      <Transaction logout={props.logout} ></Transaction>
+      <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
